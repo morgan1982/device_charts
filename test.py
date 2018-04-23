af17456177e9ae95e9b7ad36189056dcb684c7b0
entropy82 <froutopotos.alex@gmail.com>
2018-04-23 11:14:52 +0300

    init

---
+++ test.py
@@ -0,0 +1,110 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'test.ui'
+#
+# Created by: PyQt4 UI code generator 4.11.4
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt4 import QtCore, QtGui
+
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    def _fromUtf8(s):
+        return s
+
+try:
+    _encoding = QtGui.QApplication.UnicodeUTF8
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig, _encoding)
+except AttributeError:
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig)
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName(_fromUtf8("MainWindow"))
+        MainWindow.resize(871, 580)
+        self.centralwidget = QtGui.QWidget(MainWindow)
+        self.centralwidget.setObjectName(_fromUtf8("centralwidget"))
+        self.total_btn = QtGui.QPushButton(self.centralwidget)
+        self.total_btn.setGeometry(QtCore.QRect(0, 100, 75, 23))
+        self.total_btn.setObjectName(_fromUtf8("total_btn"))
+        self.price_tedit_1 = QtGui.QTextEdit(self.centralwidget)
+        self.price_tedit_1.setGeometry(QtCore.QRect(80, 0, 104, 31))
+        palette = QtGui.QPalette()
+        brush = QtGui.QBrush(QtGui.QColor(255, 0, 127))
+        brush.setStyle(QtCore.Qt.SolidPattern)
+        palette.setBrush(QtGui.QPalette.Active, QtGui.QPalette.WindowText, brush)
+        brush = QtGui.QBrush(QtGui.QColor(255, 0, 127))
+        brush.setStyle(QtCore.Qt.SolidPattern)
+        palette.setBrush(QtGui.QPalette.Inactive, QtGui.QPalette.WindowText, brush)
+        brush = QtGui.QBrush(QtGui.QColor(120, 120, 120))
+        brush.setStyle(QtCore.Qt.SolidPattern)
+        palette.setBrush(QtGui.QPalette.Disabled, QtGui.QPalette.WindowText, brush)
+        self.price_tedit_1.setPalette(palette)
+        self.price_tedit_1.setFrameShadow(QtGui.QFrame.Raised)
+        self.price_tedit_1.setObjectName(_fromUtf8("price_tedit_1"))
+        self.price_tedit_2 = QtGui.QTextEdit(self.centralwidget)
+        self.price_tedit_2.setGeometry(QtCore.QRect(80, 40, 104, 31))
+        self.price_tedit_2.setObjectName(_fromUtf8("price_tedit_2"))
+        self.price_tedit_4 = QtGui.QTextEdit(self.centralwidget)
+        self.price_tedit_4.setGeometry(QtCore.QRect(80, 120, 104, 31))
+        self.price_tedit_4.setObjectName(_fromUtf8("price_tedit_4"))
+        self.price_tedit_3 = QtGui.QTextEdit(self.centralwidget)
+        self.price_tedit_3.setGeometry(QtCore.QRect(80, 80, 104, 31))
+        self.price_tedit_3.setObjectName(_fromUtf8("price_tedit_3"))
+        self.price_tedit_5 = QtGui.QTextEdit(self.centralwidget)
+        self.price_tedit_5.setGeometry(QtCore.QRect(80, 160, 104, 31))
+        self.price_tedit_5.setObjectName(_fromUtf8("price_tedit_5"))
+        self.part_spx = QtGui.QSpinBox(self.centralwidget)
+        self.part_spx.setGeometry(QtCore.QRect(10, 0, 42, 22))
+        self.part_spx.setObjectName(_fromUtf8("part_spx"))
+        self.nr_chbox = QtGui.QCheckBox(self.centralwidget)
+        self.nr_chbox.setGeometry(QtCore.QRect(10, 30, 70, 17))
+        self.nr_chbox.setObjectName(_fromUtf8("nr_chbox"))
+        self.lc_chbox = QtGui.QCheckBox(self.centralwidget)
+        self.lc_chbox.setGeometry(QtCore.QRect(10, 50, 70, 17))
+        self.lc_chbox.setObjectName(_fromUtf8("lc_chbox"))
+        self.nr_lc_chbox = QtGui.QCheckBox(self.centralwidget)
+        self.nr_lc_chbox.setGeometry(QtCore.QRect(10, 70, 70, 17))
+        self.nr_lc_chbox.setObjectName(_fromUtf8("nr_lc_chbox"))
+        self.total_lcd = QtGui.QLCDNumber(self.centralwidget)
+        self.total_lcd.setGeometry(QtCore.QRect(110, 200, 71, 41))
+        self.total_lcd.setObjectName(_fromUtf8("total_lcd"))
+        self.pushButton_2 = QtGui.QPushButton(self.centralwidget)
+        self.pushButton_2.setGeometry(QtCore.QRect(30, 220, 75, 23))
+        self.pushButton_2.setObjectName(_fromUtf8("pushButton_2"))
+        self.plt_widget = QtGui.QWidget(self.centralwidget)
+        self.plt_widget.setGeometry(QtCore.QRect(260, 40, 451, 351))
+        self.plt_widget.setObjectName(_fromUtf8("plt_widget"))
+        self.dist_btn = QtGui.QPushButton(self.centralwidget)
+        self.dist_btn.setGeometry(QtCore.QRect(270, 400, 75, 23))
+        self.dist_btn.setObjectName(_fromUtf8("dist_btn"))
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtGui.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 871, 21))
+        self.menubar.setObjectName(_fromUtf8("menubar"))
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtGui.QStatusBar(MainWindow)
+        self.statusbar.setObjectName(_fromUtf8("statusbar"))
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QObject.connect(self.pushButton_2, QtCore.SIGNAL(_fromUtf8("clicked()")), self.price_tedit_5.clear)
+        QtCore.QObject.connect(self.pushButton_2, QtCore.SIGNAL(_fromUtf8("clicked()")), self.price_tedit_4.clear)
+        QtCore.QObject.connect(self.pushButton_2, QtCore.SIGNAL(_fromUtf8("clicked()")), self.price_tedit_3.clear)
+        QtCore.QObject.connect(self.pushButton_2, QtCore.SIGNAL(_fromUtf8("clicked()")), self.price_tedit_2.clear)
+        QtCore.QObject.connect(self.pushButton_2, QtCore.SIGNAL(_fromUtf8("clicked()")), self.price_tedit_1.clear)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow", None))
+        self.total_btn.setText(_translate("MainWindow", "get price", None))
+        self.nr_chbox.setText(_translate("MainWindow", "nr", None))
+        self.lc_chbox.setText(_translate("MainWindow", "lc", None))
+        self.nr_lc_chbox.setText(_translate("MainWindow", "nr and lc", None))
+        self.pushButton_2.setText(_translate("MainWindow", "clear", None))
+        self.dist_btn.setText(_translate("MainWindow", "dist", None))
+
---
+++ test_structure.py
@@ -0,0 +1,207 @@
+import sys
+from PyQt4 import QtGui, QtCore
+from test import Ui_MainWindow  # the saved ui file
+
+from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.backends.backend_qt4agg import NavigationToolbar2QT as NavigationToolbar
+import matplotlib.pyplot as plt
+import numpy as np
+
+import sqlite3
+import datetime
+from matplotlib.dates import datestr2num
+# import time
+# import datetime
+# import bs4 as bs
+# import urllib.request
+# from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg
+# from matplotlib.figure import Figure
+print("initializing..")
+
+
+class Main(QtGui.QMainWindow):
+
+    def __init__(self):
+        QtGui.QMainWindow.__init__(self)
+        self.ui = Ui_MainWindow()  # the ui file here
+        self.ui.setupUi(self)
+        self.statusBar().showMessage('ready')
+        self.setWindowTitle('ipad database')
+
+        # q_widget for plot
+        self.figure1 = plt.figure()
+        self.canvas1 = FigureCanvas(self.figure1)
+        # self.toolbar1 = NavigationToolbar(self.canvas1, self)
+
+        layout1 = QtGui.QVBoxLayout()
+        # layout1.addWidget(self.toolbar1)
+        layout1.addWidget(self.canvas1)
+
+        self.ui.plt_widget.setLayout(layout1)
+        self.num_rep_init()  
+
+
+    def device_selector(self, dev):
+
+        conn = sqlite3.connect('ipad.db')
+        c = conn.cursor()
+        device_list = dev
+        devices = []
+        for i in device_list:
+            c.execute("SELECT COUNT(ticket) FROM repairs WHERE device=?", (i, ))
+            item = c.fetchone()
+            print("item", item)
+            devices.append(item)
+
+        devices = [i[0] for i in devices]
+
+        c.close()
+        conn.close()  
+
+        return devices
+
+
+
+
+
+
+    def num_rep_init(self):
+
+
+        device_list = ["ipad 2", "ipad 3", "ipad 4", "ipad air", "ipad mini"]
+        devices_count = self.device_selector(device_list)
+        print(devices_count)
+
+
+        # setup graph 
+        self.ax1 = self.figure1.add_subplot(111)
+        self.ax1 = plt.subplot2grid((1, 1), (0, 0))
+        self.ax1.grid(True, color='k', linestyle='dotted', linewidth=0.5 )
+
+        width = 0.35
+
+        self.ax1.bar(device_list, devices_count, width,  color='orange', alpha= 0.5, align ='center' ) # the plot data
+        # self.ax1.bar(dates + 0.2, count_dev, width,  color='grey', alpha= 0.5 ) # the plot data
+
+        self.ax1.set_axisbelow(True)
+
+        # plt.xticks( dates)
+        # plt.xticks(rotation=45)
+        # self.ax1.set_yticks([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15])
+
+
+        plt.xlabel('devices')
+        plt.ylabel('num of repairs')
+        plt.title('dev destr')
+
+        self.canvas1.draw()
+
+
+        # c.execute(
+        #     "SELECT COUNT(datestamp) AS entries, DATE(datestamp) AS date FROM repairs WHERE DATE(datestamp) BETWEEN datetime('now', '-10 days') AND datetime('now', 'localtime') GROUP BY DATE(datestamp)")
+        # value = c.fetchone()
+        # print(value[0])
+        # dev = "ipad 4"
+        # print("values", values)
+        # c.execute("select ticket from repairs where device=?", (dev,))
+        # devices = c.fetchall()
+        # x = [i[0] for i in devices]
+        # print(len(x))
+
+        # count_dev = [x[0] for x in values]
+        # sum_count_dev = sum(count_dev)
+        # length__dev = len(count_dev)
+        # average_repairs = sum_count_dev / length__dev
+        # print(sum_count_dev)
+        # print(length__dev)
+        # print(average_repairs)  # average of repairs total
+
+        # dates = [x[1] for x in values]
+        # print(dates)
+        # dates = datestr2num(dates)
+        # print(dates)
+
+
+        # counter = 0
+        # dates = [] # the x axis ?
+        # print(dates)
+        # dates = [k[1] for k  in val  for val in dates]
+        # z = []
+        # for i in dates:
+            # for k in i:
+                # z.append (k[1])
+        # print(z)
+        # dates_x = date2num(dates)
+        # print(dates_x)
+        # counter_list = [] # the y axis?
+        # for x in values:
+        #     dates.append(x[1])
+        #     counter_list.append(counter)
+        #     counter += 1
+        # dates_new = dates.datestr2num(dates)
+        # print(count_dev)
+        # print(dates)
+        # print(counter_list) # counts the days
+
+
+
+
+        # c.close()
+        # conn.close()
+
+
+
+
+        # fig = plt.figure()
+        # ax1 = plt.subplot2grid((1, 1), (0, 0))
+        # plt.xticks(counter_list, dates)  #  to replace numbers in x with custom string we can use also yticks
+        # plt.xticks(rotation=45)
+        # # ax1.bar(counter_list, count_dev, label="devices", width=1/1.8, color="orange" )  # bars enablerL
+        # plt.plot(counter_list, count_dev, color="k")  # graph enabler
+        # ax1.grid(True, color='k', linestyle='dotted', linewidth=0.5)  # change the properties of the grid
+        # ax1.xaxis.label.set_color('k')
+        # ax1.yaxis.label.set_color('magenta')
+        # ax1.set_yticks([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15])
+        # ax1.fill_between(counter_list, 0, count_dev, color='orange', alpha=0.6)
+        # ax1.spines['left'].set_color('c')
+        # ax1.spines['right'].set_visible(False)
+        # ax1.spines['top'].set_visible(False)
+        # ax1.spines['bottom'].set_color('c')
+
+        # ax1.set_axisbelow('True')
+        # plt.xlabel('dates')
+        # '''
+
+        # to change the rotation of the dates on x axis or just plt.xticks(rotation=45)
+        # '''
+        # # for label in ax1.xaxis.get_ticklabels():
+        # #     label.set_rotation(45)
+        # plt.ylabel('repairs')
+        # plt.title('Number of repairs per day\naverage numbers')
+        # plt.legend()
+        # '''
+        # customize the view of the plot
+        # '''
+        # plt.subplots_adjust(left=0.09, bottom=0.21, right=0.94, top=0.85,
+        #                     wspace=0.2, hspace=0)
+        # plt.show()
+        # c.close()
+        # conn.close()
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    app = QtGui.QApplication(sys.argv)
+    window = Main()
+    # app.setStyleSheet(qdarkstyle.load_stylesheet(pyside=False))
+    window.show()
+    sys.exit(app.exec_())
